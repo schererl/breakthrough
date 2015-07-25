136e7390987e0596bf78148abef0d96f8637814b
Tom Pepels <tpepels@gmail.com>
2015-07-25 23:59:00 +0200

    qb

--- src/framework/Options.java
+++ src/framework/Options.java
@@ -7,7 +7,7 @@ import java.util.Random;
  */
 public class Options {
     public static final Random r = new Random();
-    public double C = .4, imAlpha = 0.1, etWv = 1.3, kq = 0.4;
+    public double C = .4, imAlpha = 0.1, etWv = 1.3, kq = 2.0;
     public boolean debug = true, fixSimulations = false,
             heuristics = false, earlyTerm = false, solver = false,
             nodePriors = false, imm = false, test = true, qualityBonus = false;
