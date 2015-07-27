ec0e2e65f4508fd4c11ad0ffdcee9902c29c881a
Tom Pepels <tpepels@gmail.com>
2015-07-27 11:39:07 +0200

    oops

--- src/breakthrough/Game.java
+++ src/breakthrough/Game.java
@@ -21,8 +21,10 @@ public class Game {
         options1.heuristics = true;
         options1.earlyTerm = true;
         options1.nodePriors = true;
-        options1.imm = true;
+//        options1.imm = true;
         options1.qualityBonus = true;
+        options1.test = true;
+        options1.etT = 20;
         aiPlayer1.setOptions(options1);
 
         AIPlayer aiPlayer2 = new UCTPlayer();
@@ -30,7 +32,7 @@ public class Game {
         options2.heuristics = true;
         options2.earlyTerm = true;
         options2.nodePriors = true;
-        options2.imm = true;
+//        options2.imm = true;
         options2.qualityBonus = true;
         aiPlayer2.setOptions(options2);
 
